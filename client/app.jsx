995e1cb32ee25115f7e1b3fcdf6905e8f949e6da
Camryn Reid <camrr1991@gmail.com>
2019-12-02 14:33:09 -0800

    Renders react component

--- client/app.jsx
+++ client/app.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class App extends React.Components {
+class App extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      restaurant: null
+    }
   }
 
-  render() {(
-    <div>React is rendering!!!!</div>
-  )};
+  render() {
+    return (
+      <div>React IS rendering!!</div>
+    )
+  }
 }
+
+ReactDOM.render(<App />, document.getElementById("app"));
--- server/server.js
+++ server/server.js
@@ -4,10 +4,6 @@ const port = 3003;
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-})
-
 app.listen(port, () => {
   console.log(`Listening on port ${port}...`);
 })
